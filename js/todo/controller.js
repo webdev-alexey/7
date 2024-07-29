9973bb653d3a7231e97cf0870f70c3e759f2fd13
Alexey <alexey.anasov@yandex.by>
2024-07-29 17:21:38 +0300

    рендер id в разметку из модели и получение id при клике по элементу

--- js/todo/controller.js
+++ js/todo/controller.js
@@ -13,5 +13,7 @@ view.elements.form.addEventListener("submit", function (e) {
 
 view.elements.tasksList.addEventListener("click", function (e) {
   if (e.target.getAttribute("type") === "checkbox") {
+    const id = e.target.closest(".todo-item").dataset.id;
+    console.log(id);
   }
 });
